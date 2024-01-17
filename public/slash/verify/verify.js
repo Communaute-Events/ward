cd73d1e0cedb62ea3d177ffc6e37ca059ca07c44
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-01-17 19:02:10 +0100

    Fixed Import

--- public/slash/verify/verify.js
+++ public/slash/verify/verify.js
@@ -1,5 +1,5 @@
 
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const config = require("../../../config.js");
 const pool = require("../../../pool.js")
 
