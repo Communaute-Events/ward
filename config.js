995ba4b3f25fa61fdc4ed1c7de7bbf4c96835f0b
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-07-08 22:10:49 +0200

    Switched to doofenbrains server

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ module.exports = {
     },
 
     Discord: {
-        verifiedRole: "1184925847078055958", // —— Role that will be added to the user when they verify their account.
+        verifiedRole: "1259963837013168189", // —— Role that will be added to the user when they verify their account.
 
         // —— For users that want to have a role removed upon verification, if you want this, set remove-role to true, and set your remove role ID.
-        removeRole: true,
+        removeRole: false,
         removeRoleId: "1206307358624579584",
 
         // —— Set the bot's presence, for statusType see: https://discord-api-types.dev/api/discord-api-types-v10/enum/ActivityType
         statusType: 0, // 1 (STREAMING), 2 (LISTENING), 3 (WATCHING), 5 (COMPETING). Default is 0 (PLAYING). 
-        statusMsg: "à protéger communaué events",
+        statusMsg: "brainstorming",
 
         // —— By default, rules are set to disabled, this means rules will be hidden. If you want to use the rules function, change disabled to your rules. Please ensure you use \n for each line break and do not use any symbols that could interfear with JSON.
         rulesEnabled: false,
